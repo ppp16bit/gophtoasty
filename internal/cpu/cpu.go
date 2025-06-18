cf5581edde8a02a2d7121efa3add0a7f621862ec
ppp16bit <pcampossobrinho@gmail.com>
2025-06-18 18:17:52 -0300

    [feat] functions to get and print cpu temperature

---
+++ internal/cpu/cpu.go
@@ -0,0 +1,36 @@
+package cpu
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/shirou/gopsutil/v3/host"
+)
+
+func GetCPUTemp() (float64, error) {
+	sensors, err := host.SensorsTemperatures()
+
+	if err != nil {
+		return 0, fmt.Errorf("Error: %w", err)
+	}
+
+	/* if your motherboards ACPI temperature sensor is different
+	please replace it here */
+	for _, sensor := range sensors {
+		if strings.Contains(sensor.SensorKey, "acpitz") {
+			return sensor.Temperature, nil
+		}
+	}
+
+	return 0, fmt.Errorf("Not found")
+}
+
+func PrintCPUTemp() {
+	temp, err := GetCPUTemp()
+
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+
+	fmt.Printf("Current CPU temperature: %.2f°C\n", temp)
+}
