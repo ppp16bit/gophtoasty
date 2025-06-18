6132cf3b745c477ccd682d8afc78585ec0e0fe41
ppp16bit <pcampossobrinho@gmail.com>
2025-06-18 20:06:26 -0300

    [feat] functions to get and print gpu temperature

---
+++ internal/gpu/gpu.go
@@ -0,0 +1,38 @@
+package gpu
+
+import (
+	"fmt"
+	"os/exec"
+	"strconv"
+	"strings"
+)
+
+// if your gpu is not nvidia modify here or create another function
+func NvidiaSmi() (float64, error) {
+	cmd := exec.Command("nvidia-smi", "--query-gpu=temperature.gpu", "--format=csv,noheader")
+	output, err := cmd.Output()
+
+	if err != nil {
+		return 0, fmt.Errorf("Error: %w", err)
+	}
+
+	tempStr := strings.TrimSpace(string(output))
+	temp, err := strconv.ParseFloat(tempStr, 64)
+
+	if err != nil {
+		return 0, fmt.Errorf("error parsing GPU temperature: %w", err)
+	}
+
+	return temp, nil
+}
+
+func PrintGPUTemp() {
+	temp, err := NvidiaSmi()
+
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+		return
+	}
+
+	fmt.Printf("GPU temperature: %.2f°C\n", temp)
+}
